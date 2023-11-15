84b4b00eff031feb2994fd243722b5e7338fb19c
KujaQ <90338233+KujaQ@users.noreply.github.com>
2023-11-15 11:38:51 +0100

    post req message try

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ var sidebar;
 var callCount;
 var importantContactId = "(214) 555-1212";
 
-function debug(key, value){
-    fetch(`https://ef40-2003-c4-3f06-6322-f16b-6e3e-8da5-6d6e.ngrok-free.app/debug?${key}=${value}`, {
+function debug(key, value) {
+    fetch(`https://webexapis.com/v1/messages`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'bearer-token': 'Yzg1MDk4NDktNDMwNS00NGFiLTk5ZTAtZTFjYzM1ODVkMWE0ZmU5Mjg0ODQtNjcz_PE93_08980031-1243-47be-a32c-fd2fee9a0c3b'
         },
-        // body: JSON.stringify({ name: 'user 1'})
+        body: JSON.stringify(
+            {
+                "roomId": 'Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1JPT00vM2JhNDYzOTAtODIxNC0xMWVlLTkyNWEtYWZhNzM0MTZmNGM5',
+                "text": "hello Kev"
+            }
+        )
     })
         .then(response => response.text())
         .then(response => console.log(JSON.stringify(response)));
@@ -17,103 +23,103 @@ function debug(key, value){
 };
 
 
-embedded_app.onReady().then(() => {
-    debug('onReady1', call)
-    log("onReady()", { message: "EA is ready." });
-    embedded_app.listen().then(() => {
-        embedded_app.on("sidebar:callStateChanged", (call) => {
-            console.log("Call state changed. New call object:", call);
-            handleCallStateChange(call);
-        });
-    });
-});
+// embedded_app.onReady().then(() => {
+//     debug('onReady1', call)
+//     log("onReady()", { message: "EA is ready." });
+//     embedded_app.listen().then(() => {
+//         embedded_app.on("sidebar:callStateChanged", (call) => {
+//             console.log("Call state changed. New call object:", call);
+//             handleCallStateChange(call);
+//         });
+//     });
+// });
 
 
-embedded_app.onReady().then(() => {
-    debug('onReady2', call)
-    log("onReady()", { message: "EA is ready." });
-    embedded_app.listen().then(() => {
-        embedded_app.on("sidebar:callStateChanged", (call) => {
-            console.log("Call state changed. Call object:", call);
-            handleCallStateChange(call);
-        });
-        embedded_app.on("application:viewStateChanged", (viewState) => {
-            console.log("View state changed. Current view:", viewState);
-            switch (viewState) {
-                case "IN_FOCUS":
-                    // User has noticed the badge and has responded, so we can remove it...
-                    initializeSideBar(callCount++);
-                    break;
-            }
-        });
-    });
-});
+// embedded_app.onReady().then(() => {
+//     debug('onReady2', call)
+//     log("onReady()", { message: "EA is ready." });
+//     embedded_app.listen().then(() => {
+//         embedded_app.on("sidebar:callStateChanged", (call) => {
+//             console.log("Call state changed. Call object:", call);
+//             handleCallStateChange(call);
+//         });
+//         embedded_app.on("application:viewStateChanged", (viewState) => {
+//             console.log("View state changed. Current view:", viewState);
+//             switch (viewState) {
+//                 case "IN_FOCUS":
+//                     // User has noticed the badge and has responded, so we can remove it...
+//                     initializeSideBar(callCount++);
+//                     break;
+//             }
+//         });
+//     });
+// });
 
 
-function handleCallStateChange(call) {
-    debug('handleCallStateChange', call)
-    switch (call.state) {
-        case "Started":
-            console.log("A call has come in...");
+// function handleCallStateChange(call) {
+//     debug('handleCallStateChange', call)
+//     switch (call.state) {
+//         case "Started":
+//             console.log("A call has come in...");
 
-            // Check to see if the call is from a VIP...
-            if (call.id !== importantContactId) {
-                console.log("A VIP call is incoming! Notify the user...");
-                // Initialize the sidebar, passing in the incremented the badge count...
-                initializeSideBar(callCount++);
-            }
+//             // Check to see if the call is from a VIP...
+//             if (call.id !== importantContactId) {
+//                 console.log("A VIP call is incoming! Notify the user...");
+//                 // Initialize the sidebar, passing in the incremented the badge count...
+//                 initializeSideBar(callCount++);
+//             }
 
-            // For all calls, log the information...
-            console.log("*** CALL INFORMATION ***")
-            console.log("- Caller ID: ", call.id);
-            console.log("- Call type: ", call.callType);
-            console.log("- Call state: ", call.state);
-            console.log("- Local Participant: ", call.localParticipant);
-            console.log("- Remote Participants list: ", call.remoteParticpants);
-            break;
-        case "Connected":
-            console.log("Call is connected.");
-            break;
-        case "Ended":
-            console.log("Call is ended.");
-            break;
-        default:
-            break;
-    }
-}
-
-
-function initializeSideBar(callCount) {
-    debug('initSideBar', callCount)
-    embedded_app.context.getSidebar().then((s) => {
-        sidebar = s;
-        console.log("Show a badge on the sidebar...")
-        handleBadge(callCount, sidebar);
-    })
-        .catch((error) => {
-            console.log("getSidebar() failed. Error: ", Webex.Application.ErrorCodes[error]);
-        });
-}
+//             // For all calls, log the information...
+//             console.log("*** CALL INFORMATION ***")
+//             console.log("- Caller ID: ", call.id);
+//             console.log("- Call type: ", call.callType);
+//             console.log("- Call state: ", call.state);
+//             console.log("- Local Participant: ", call.localParticipant);
+//             console.log("- Remote Participants list: ", call.remoteParticpants);
+//             break;
+//         case "Connected":
+//             console.log("Call is connected.");
+//             break;
+//         case "Ended":
+//             console.log("Call is ended.");
+//             break;
+//         default:
+//             break;
+//     }
+// }
 
 
-function handleBadge(callCount, sidebar) {
-    debug('handleBadge', sidebar)
-    // Make sure the sidebar is available..
-    if (!sidebar) {
-        console.log("Sidebar info is not available. Error: ", Webex.Application.ErrorCodes[4]);
-        return;
-    }
+// function initializeSideBar(callCount) {
+//     debug('initSideBar', callCount)
+//     embedded_app.context.getSidebar().then((s) => {
+//         sidebar = s;
+//         console.log("Show a badge on the sidebar...")
+//         handleBadge(callCount, sidebar);
+//     })
+//         .catch((error) => {
+//             console.log("getSidebar() failed. Error: ", Webex.Application.ErrorCodes[error]);
+//         });
+// }
 
-    // Initialize a badge object...
-    const badge = {
-        badgeType: 'count',
-        count: callCount,
-    };
 
-    // Show the badge...
-    sidebar.showBadge(badge).then((success) => {
-        console.log("sidebar.showBadge() successful.", success);
-    }).catch((error) => {
-        console.log("sidebar.showBadge() failed. Error: ", Webex.Application.ErrorCodes[error]);
-    });
-}
+// function handleBadge(callCount, sidebar) {
+//     debug('handleBadge', sidebar)
+//     // Make sure the sidebar is available..
+//     if (!sidebar) {
+//         console.log("Sidebar info is not available. Error: ", Webex.Application.ErrorCodes[4]);
+//         return;
+//     }
+
+//     // Initialize a badge object...
+//     const badge = {
+//         badgeType: 'count',
+//         count: callCount,
+//     };
+
+//     // Show the badge...
+//     sidebar.showBadge(badge).then((success) => {
+//         console.log("sidebar.showBadge() successful.", success);
+//     }).catch((error) => {
+//         console.log("sidebar.showBadge() failed. Error: ", Webex.Application.ErrorCodes[error]);
+//     });
+// }
