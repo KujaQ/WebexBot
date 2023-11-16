961a42b85c15039c911a2a5ad6d73065e0ebdfe5
Leseim <stephenMiesel@gmail.com>
2023-11-16 11:20:30 +0100

    ttt

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.onReady().then(() => {
     log("Banan 4 scale", {message: "listener läuft 1"})
     handleGetSidebar();
 
-    app.on("sidebar:callStateChanged", (payload) =>
+    app.on('sidebar:callStateChanged', (payload) =>
       log("Call state changed. New call object:", payload)
     );
 
