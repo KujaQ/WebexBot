3759a8c8118f53072d6324d7ea37427939d83c99
Leseim <stephenMiesel@gmail.com>
2023-11-16 10:36:32 +0100

    jh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ parseJwtFromURLHash();
 
 const app = new window.webex.Application();
 //app = new window.webex.Application();
-var sidebar, meetings;
+//var sidebar, meetings;
 
 
 app.onReady().then(() => {
