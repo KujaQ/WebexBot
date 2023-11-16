43816602bc86d3c9c31accec8a84b63ce0d4f23c
Leseim <stephenMiesel@gmail.com>
2023-11-16 10:39:45 +0100

    rrtr

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Check URL Hash for Login with Webex Token
 parseJwtFromURLHash();
 
-const app = new window.webex.Application();
+const app = new window.Webex.Application();
 //app = new window.webex.Application();
 //var sidebar, meetings;
 
